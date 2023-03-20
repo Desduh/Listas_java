fccb6d1d01ea53431947f4485190a0fdbfa64be5
Desduh <eduardofalandess@gmail.com>
2023-03-20 20:37:55 -0300

    correcao de arquivos

---
+++ atvi/automanager/src/main/java/com/autobots/automanager/modelo/DocumentoExclusor.java
@@ -0,0 +1,25 @@
+package com.autobots.automanager.modelo;
+
+import java.util.List;
+import com.autobots.automanager.entidades.Documento;
+
+public class DocumentoExclusor {
+	public int excluir(Documento documento, List<Documento> documentos) {
+		int index = documentos.indexOf(documento);
+		return index;
+	}
+
+	public int excluir(List<Documento> documentos, List<Documento> atualizacoes) {
+		for (Documento atualizacao : atualizacoes) {
+			for (Documento documento : documentos) {
+				if (atualizacao.getId() != null) {
+					if (atualizacao.getId() == documento.getId()) {
+						int index = excluir(documento, documentos);
+						return index;
+					}
+				}
+			}
+		}
+		return -1;
+	}
+}
---
+++ atvi/automanager/src/main/java/com/autobots/automanager/modelo/Exclusor.java
@@ -0,0 +1,29 @@
+package com.autobots.automanager.modelo;
+
+import java.util.List;
+
+import com.autobots.automanager.entidades.Cliente;
+import com.autobots.automanager.entidades.Documento;
+import com.autobots.automanager.entidades.Telefone;
+
+public class Exclusor {	
+	public void excluirDocumentos(Cliente cliente, List<Documento> documentos) {
+		DocumentoExclusor exclusor = new DocumentoExclusor();
+		int index = exclusor.excluir(cliente.getDocumentos(), documentos);
+		if(index != -1) {
+			cliente.getDocumentos().remove(index);
+		}
+	}
+	public void excluirTelefones(Cliente cliente, List<Telefone> telefones) {
+		TelefoneExclusor exclusor = new TelefoneExclusor();
+		int index = exclusor.excluir(cliente.getTelefones(), telefones);
+		if(index != -1) {
+			cliente.getTelefones().remove(index);
+		}
+	}
+	
+	public void excluir(Cliente cliente, Cliente exclusao) {
+		excluirDocumentos(cliente, exclusao.getDocumentos());
+		excluirTelefones(cliente, exclusao.getTelefones());
+	}
+}
